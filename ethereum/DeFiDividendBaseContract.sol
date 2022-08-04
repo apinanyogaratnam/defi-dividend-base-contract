0b78f42dbf7450d62b1ac03b0211cb1409deed27
apinanyogaratnam <apinanapinan@icloud.com>
2022-08-04 14:30:47 -0400

    feat: created basic contract boilerplate

---
+++ ethereum/DeFiDividendBaseContract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract DeFiDividendBaseContract {
+    mapping(address => uint) stakedDate;
+    uint dividendRate = 10;
+
+    function getStakedDate(address _address) public view returns (uint) {
+        return stakedDate[_address];
+    }
+}
